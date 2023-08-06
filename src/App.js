d646b3b923eb889a36a9a94d1ed9a4e3579ad604
Abhishek7487 <dellabhishek7487@gmail.com>
2023-08-06 14:54:23 +0530

    Initialized react-quiz-app with basic skelton of the app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
